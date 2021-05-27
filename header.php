266849c5ca2dc7c0827c1eedfee21c6a7e084586
saumiljoshi <84835792+saumiljoshi@users.noreply.github.com>
2021-05-27 16:32:57 +0530

    store in web folder

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>attendance system- php crud using oops and mvc</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+    <ul class="nav nav tabs">
+    <li><a href= "index.php" class="active">student</a></li>
+    <li><a href= "index.php?action-attendance">Attendance</a></li>
+    <li><a href= "#" style = "color:red;">welcome guest</a></li>
+    </ul>
+    </div>
+    <img src="web/image/attendancelogo.png" width="300" height="100" alt="no-image" style="margin-left 400px; margin-top: 20 px" />
+</body>
+</html>
