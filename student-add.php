290ca171afcf0ca2a47516fec0ab26a9949fbcdf
saumiljoshi <84835792+saumiljoshi@users.noreply.github.com>
2021-05-27 16:26:16 +0530

    Add files via upload

---
+++ student-add.php
@@ -0,0 +1,26 @@
+<?php 
+require_once("web/header.php");
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <div>
+ <form>
+ <label for="roll_number">roll_number</label>
+ <input type="text" name="roll_number" required class="form-control"/>
+ <label for="name">name</label>
+ <input type="text" name="name" required class="form-control"/>
+ <label for="class">class</label>
+ <input type="text" name="class" required class="form-control"/>
+ </form>
+ </div>
+</body>
+</html>
